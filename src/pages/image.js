adde06f247fa8e53ae1156afce839d590c4d7f80
Andrew Hudson <ahudson@gmail.com>
2018-09-26 21:48:03 +0100

    naming

--- src/pages/image.js
+++ src/pages/image.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Layout from '../components/layout'
-import ImageBlock from '../components/ImageBlock'
+import ImageBlock from '../components/imageBlock'
 
 class ImagePage extends React.Component {
 	constructor(props) {
